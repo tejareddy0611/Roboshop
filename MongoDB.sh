6c6e69618a0161d518635c5aea348a73da34222b
tejareddy0611 <tejareddy0611@gmail.com>
2023-12-30 02:34:17 -0600

    Second commit

--- MongoDB.sh
+++ MongoDB.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$ (date +%F -%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
