ac2c34025f8533e56aaad3f4b9aaa79b949453fc
tejareddy0611 <tejareddy0611@gmail.com>
2023-12-30 04:31:05 -0600

    Second commit

--- MongoDB.sh
+++ MongoDB.sh
@@ -12,13 +12,13 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE () {
-       if [ $1 then -ne 0 ];
-       then
-        echo -e "$2 ...$R FAILED $N"
-       else
-        echo -e "$2 ...$G SUCCESS $N"
-       fi
+   if [ "$1" -ne 0 ]; then
+      echo -e "$2 ...$R FAILED $N"
+   else
+      echo -e "$2 ...$G SUCCESS $N"
+   fi
 }
+
 
 
 if [ $ID -ne 0 ];
@@ -33,4 +33,23 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
 
 VALIDATE $? "Copied mongo repos"
 
+dnf install mongodb-org -y &>> LOGFILE
+
+VALIDATE $? "Installes mongodb-org"
+
+systemctl enable mongod &>> LOGFILE
+
+VALIDATE $? "enable mongodb"
+
+systemctl start mongod  &>> LOGFILE
+
+VALIDATE $? "start mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' etc/mongod.conf &>> LOGFILE
+
+VALIDATE $? "Remote access to Mongodb"
+
+systemctl restart mongod &>> LOGFILE
+
+VALIDATE $? "Restarting mongodb"
 
