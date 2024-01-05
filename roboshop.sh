a84ffdcd28690ebdc8780a84d094d7d35eb8852f
tejareddy0611 <tejareddy0611@gmail.com>
2024-01-05 04:20:45 -0600

    second command

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+SG_ID=sg-07f715e044e24890d #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z04759193TE6S0ZO478Y9 # replace your zone ID
 DOMAIN_NAME="awsdevopsonline.website"
