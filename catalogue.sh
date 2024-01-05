0463ec420211e66cf52aabec278d852437cf3ecc
tejareddy0611 <tejareddy0611@gmail.com>
2024-01-05 04:12:59 -0600

    second command

--- catalogue.sh
+++ catalogue.sh
@@ -16,6 +16,7 @@ echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE () {
    if [ "$1" -ne 0 ]; then
       echo -e "$2 ...$R FAILED $N"
+      exit 1
    else
       echo -e "$2 ...$G SUCCESS $N"
    fi
